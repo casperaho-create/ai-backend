0b628698a5ec40535e34478fc9f3760823d29b3a
casperaho-create <casper.aho@gmail.com>
2026-03-03 11:27:31 +0100

    Update reply.js

--- api/admin/reply.js
+++ api/admin/reply.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
     }
 
     await resend.emails.send({
-      from: "AI Support <onboarding@resend.dev>",
+      from: "AI Support <kontakt@dindomän.se>",
       to,
       subject: "Svar på din förfrågan",
       html: `
